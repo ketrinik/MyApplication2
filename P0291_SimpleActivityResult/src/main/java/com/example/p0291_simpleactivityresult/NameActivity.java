6b217f8b6a5762e15a4b3365f9f31941ad1a1e97
ketrinik <ketrinik@gmail.com>
2018-08-30 18:32:43 +0800

    Add new module P0291_SimpleActivityResult

---
+++ P0291_SimpleActivityResult/src/main/java/com/example/p0291_simpleactivityresult/NameActivity.java
@@ -0,0 +1,33 @@
+package com.example.p0291_simpleactivityresult;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NameActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText etName;
+    Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.name);
+
+        etName = (EditText) findViewById(R.id.etName);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.putExtra("name", etName.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+}
